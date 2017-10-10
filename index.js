d9fa43ea9473d9099ccb8a4a5db7acb9e9660fbe
green <green@orlov.com.ua>
2017-10-10 22:01:28 +0300

    fixes

---
+++ index.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var course = require('./data');
+
+https.get('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3', function (res) {
+   res.on('data', function (data) {
+      fs.writeFile('data.json', data, function () {
+         console.log('File is writed!');
+      });
+   });
+}).on('error', function (err) {
+   console.log(err);
+});
+
+console.log(
+ "Гривня к евро: \nПродать: " + course[0].buy + "\n" + "Купить: " + course[0].sale + "\n========================\n" +
+ "Гривня к рублю: \nПродать: " + course[1].buy + "\n" + "Купить: " + course[1].sale + "\n========================\n" +
+ "Гривня к доллару: \nПродать: " + course[2].buy + "\n" + "Купить: " + course[2].sale + "\n========================\n"
+);
